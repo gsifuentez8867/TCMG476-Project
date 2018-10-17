6ed84d95886dc628849ce8651d0232a809a4e04c
gsifuentez8867 <gsifuentez8867@tamu.edu>
2018-10-16 21:32:27 -0500

    file parsing

--- project.py
+++ project.py
@@ -5,3 +5,14 @@ import re
 URL_PATH= 'https://s3.amazonaws.com/tcmg476/http_access_log'
 LOCAL_FILE = 'local_copy.log'
 local_file, headers = urlretrieve(URL_PATH, LOCAL_FILE)
+
+
+ERRORS = []
+
+regex = re.compile(".*\[([^:]*):(.*) \-[0-9]{4}\] \"([A-Z]+) (.+?)( HTTP.*\"|\") ([2-5]0[0-9]) .*")
+
+
+fileparse= open(LOCAL_FILE)
+for line in fileparse:
+	current=fileparse.readline()
+	parts= regex.split(current)
